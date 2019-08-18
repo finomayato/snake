42f668aee79d8c3bc0230e334ceb490108227684
Vadim Yanko <yanko.vadim@gmail.com>
2019-08-18 19:50:09 +0300

    capturing arrow keys

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ impl App {
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
         const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
-        let square = rectangle::square(0.0, 0.0, 50.0);
+        let square = rectangle::square(0.0, 0.0, 10.0);
         let rotation = self.rotation;
         let (x, y) = (args.window_size[0] / 2.0,
                       args.window_size[1] / 2.0);
@@ -58,6 +58,17 @@ fn main() {
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
+        
+        if let Some(Button::Keyboard(key)) = e.press_args() {
+            match key {
+                Key::Left => println!("Going left!"),
+                Key::Right => println!("Going right!"),
+                Key::Up => println!("Goint up!"),
+                Key::Down => println!("Going down!"),
+                _ => ()
+            }
+        }
+
         if let Some(r) = e.render_args() {
             app.render(&r);
         }
